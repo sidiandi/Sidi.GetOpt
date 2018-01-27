7436f6a15e6a2fad14207d460e5c7c3753f6b315
Andreas Grimme <andreas.grimme@siemens.com>
2018-01-27 10:33:52 +0100

    initial version

---
+++ build/NugetRestore.cs
@@ -0,0 +1,47 @@
+using System;  
+using Microsoft.Build.Framework;  
+
+public class NugetRestore: ITask  
+{  
+    //When implementing the ITask interface, it is necessary to  
+    //implement a BuildEngine property of type  
+    //Microsoft.Build.Framework.IBuildEngine. This is done for  
+    //you if you derive from the Task class.  
+    private IBuildEngine buildEngine;  
+    public IBuildEngine BuildEngine  
+    {  
+        get  
+        {  
+            return buildEngine;  
+        }  
+        set  
+        {  
+            buildEngine = value;  
+        }  
+        }  
+
+    // When implementing the ITask interface, it is necessary to  
+    // implement a HostObject property of type Object.  
+    // This is done for you if you derive from the Task class.  
+    private ITaskHost hostObject;  
+    public ITaskHost HostObject  
+    {  
+        get  
+        {  
+            return hostObject;  
+        }  
+
+        set  
+        {  
+            hostObject = value;  
+        }  
+    } 
+
+    public string SolutionFile { get; set; } 
+
+    public bool Execute()  
+    {  
+        // This is where the task would presumably do its work.  
+        return true;  
+    }  
+}  
